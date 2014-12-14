770236ecf4aeb3e7009d1b8d7e6311c37c141375
EndIess <s7070115@kmitl.ac.th>
2014-12-14 11:32:42 +0700

    change GUI

--- tkkk2.py
+++ tkkk2.py
@@ -1,24 +1,11 @@
 from tkinter import *
-from PIL import Image, ImageTk
-import tkinter as tk
 root = Tk()     
 top = Frame(root)
-top.grid(columnspan=3, rowspan=2)
+top.grid(column=0, row=0)
 
-##windows
-root.title("MAGIC WORD")
-root.resizable(width=FALSE, height=FALSE)
-root.config(bg='#414141')
-
-image = Image.open("logo.gif")
-photo = ImageTk.PhotoImage(image)
-label = tk.Label(image=photo)
-label.image = photo 
-label.grid(column=0, row=5, padx=25, pady= 15)
-
-
-def change(string, temp = ''):
-    """eng -> thai"""
+def change(self, temp = ''):
+    """eng <-> thai"""
+    string = r.get()
     str1 = '1234567890-=qwertyuiop[]asdfghjkl;\'zxcvbnm,./'
     str2 = 'ๅ/-ภถุึคตจขชๆไำพะัีรนยบลฟหกดเ้่าสวงผปแอิืทมใฝ'
     str3 = '!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?'
@@ -33,39 +20,41 @@ def change(string, temp = ''):
         elif i in str4:
             temp += str3[str4.find(i)]
         else:temp += i
-    return(temp)
+    s.set(temp)
+    root.clipboard_clear()
+    root.clipboard_append(temp)
 
-####make "MAGIC WORD"
-##hwtext = Label(top, text='MAGIC WORD', font='times 18 bold', bg='#414141')
-##hwtext.grid(column=0, row=1, columnspan=2)
+##make "MAGIC WORD"
+gif1 = PhotoImage(file = 'logo.gif')
+canvas = Canvas(top,width=295,height=170)
+canvas.create_image(150, 0, image = gif1, anchor = N)
+canvas.grid(column=0, row=0, columnspan=3, rowspan=2)
 
 ##print enter the sentense
-mntext = Label(top, text='Enter The Sentense', font='tohoma 9', bg='#414141')
-mntext.grid(column=0, row=1, sticky=E)
+mntext = Label(top, text='Enter The Sentense', font='tohoma 9')
+mntext.grid(column=0, row=2)
 
 ##print result
-nntext = Label(top, text='Result', font='tohoma 9', bg='#414141')
-nntext.grid(column=0, row=2, sticky=E)
+nntext = Label(top, text='Result', font='tohoma 9')
+nntext.grid(column=0, row=3, sticky=E)
 
 ##input
 r = StringVar()
-r_entry = Entry(top, textvariable=r, bg='#ff8040')
-r_entry.grid(column=1, row=1)
+r_entry = Entry(top, textvariable=r, width=30)
+r_entry.grid(column=1, row=2)
 
 ##output
 s = StringVar()
-s_entry = Entry(top, textvariable=s, bg='#ff8040')
-s_entry.grid(column=1, row=2)
+s_entry = Entry(top, textvariable=s, width=30)
+s_entry.grid(column=1, row=3)
 
 ##button
 def pressed():
+    r_entry.bind('<Return>', change)
     try:
         if r.get() != root.clipboard_get() and s.get() != root.clipboard_get():
             r.set(root.clipboard_get())
-            string = change(r.get())
-            s.set(string)
-            root.clipboard_clear()
-            root.clipboard_append(string)
+            change('a')
     except:
         pass
     root.after(10, pressed)
@@ -75,10 +64,8 @@ def quit(event=None):
     root.destroy()
 
 quit_button = Button(text='END PROGRAM', command=quit,
-                     background='#ff5b5b', foreground='#ffffff',height=1, width=33)
+                     background='#ff5b5b', foreground='#ffffff',height=1, width=41)
 quit_button.columnconfigure(0, weight=1)
-quit_button.grid(row=4)
-
-##root.bind_all('<q>', quit)
+quit_button.grid(row=6)
 root.after(0, pressed)
 root.mainloop()
